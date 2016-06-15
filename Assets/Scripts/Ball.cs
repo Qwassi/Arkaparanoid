7503b7a7003349c7832391edf4fe55efd49b8bee
Qwassi <sulha_ps3@yahoo.com>
2016-06-15 22:14:51 +0800

    Nearly Finish Chapter 3
    Just need to add collider to blocks so that they can be hit by the ball.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+
+	public Paddle paddle;
+	private bool gameStarted = false;
+	private Vector3 paddleVector;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//Put the paddle right under the ball during game start
+		//by using relative to paddle position
+		paddleVector = this.transform.position - paddle.transform.position;
+
+		//Put the paddle right under the ball during game start
+		//by using manually assigned values for the paddleVector
+		//paddleVector = new Vector3(-0.4f, 0.3f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!gameStarted) {
+			this.transform.position = paddle.transform.position + paddleVector;
+			if (Input.GetMouseButtonDown (0)) {
+				print ("Mouse clicked!");
+				gameStarted = true;
+				this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (2f, 10f);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour
+{
+	public int i = -200;
+
+	// Use this for initialization
+	void Start ()
+	{
+		print ("This is my attempt number " + i);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//Get current paddle position
+		Vector3 paddlePos = new Vector3 (8f, this.transform.position.y, 0f);
+
+		//Get mouse position
+		float mousePos = (Input.mousePosition.x / Screen.width * 16)-0.5f;
+
+		//Set new mouse X position for the background with x = -1
+		//paddlePos.x = Mathf.Clamp (mousePos, -0.3f, 15.4f);
+
+		//Set new mouse X position
+		//Below is the most correct values, which is for the background x = 0
+		paddlePos.x = Mathf.Clamp (mousePos, 0.72f, 15.4f);
+
+		this.transform.position = paddlePos;
+	}
+}
