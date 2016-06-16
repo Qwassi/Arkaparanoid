bb9284801c7c07327c57539e07399c9936249d6c
Qwassi <sulha_ps3@yahoo.com>
2016-06-16 15:40:25 +0800

    In Chapter 4, scripting the game manager

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour
+{
+
+	public int timesHit;
+	public int maxHit;
+
+	// Use this for initialization
+	void Start ()
+	{
+		timesHit = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		print ("Brick got hit!");
+		timesHit++;
+		if (timesHit == maxHit) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//All possible game states
+public enum GameState
+{
+	NotStarted,
+	Playing,
+	Completed,
+	Failed
+}
+
+//Require an audio source for the object
+[RequireComponent(typeof(AudioSource))]
+
+public class GameManager : MonoBehaviour
+{
+	//Sounds to be played when one of the game states is entered
+	public AudioClip StartSound;
+	public AudioClip FailedSound;
+
+	//Starts the game with "NotStarted" game state
+	private GameState currentState = GameState.NotStarted;
+
+	//Number of all bricks and balls available in the game
+	//Used to track how many of them are left
+	private Brick[] allBrick;
+	private Ball[] allBall;
+	private Paddle paddle;
+
+	public float Timer = 0.0f;
+	private int minutes;
+	private int seconds;
+	public string formattedTime;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Time.timeScale = 1;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
